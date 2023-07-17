fdc32ea17f8181d29a921538740102c46e46f08d
Pedro <pedrohalmeidamendonca@gmail.com>
2023-07-17 14:39:37 -0300

    Requisito 02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="1",
+        product_name="Test Product",
+        company_name="Test Company",
+        manufacturing_date="2023-07-17",
+        expiration_date="2023-12-31",
+        serial_number="1793ZTM",
+        storage_instructions="Lorem ipsum dolor sit amet."
+    )
+
+    assert str(product) == (
+        "The product 1 - Test Product "
+        "with serial number 1793ZTM "
+        "manufactured on 2023-07-17 "
+        "by the company Test Company "
+        "valid until 2023-12-31 "
+        "must be stored according to the following instructions: "
+        "Lorem ipsum dolor sit amet.."
+    )
